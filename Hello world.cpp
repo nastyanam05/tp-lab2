96b301ec532288d1de24d3c9422229146ade46c4
Nastya Naumova <112873954+nastyanam05@users.noreply.github.com>
2023-05-16 13:00:26 +0300

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -3,9 +3,9 @@ include <string>
 int main() {
 cout << "Hello world!";
 string name;
-// user name
+// user n.
 cout << "Please enter name";
 cin >> name;
-// input user name
+// введите данные
 cout << "Hello world from " << name;
 }
