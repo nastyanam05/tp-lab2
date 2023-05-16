7456bd3e9032290147ea03e6a65aa7f3eaa5d4f1
Nastya Naumova <nastia.nnn005@gmail.com>
2023-05-16 12:56:30 +0300

    changed codestyle Added h1 tag

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,11 +1,11 @@
-include <iostream>
-include <string>
-int main() {
-cout << "Hello world!";
-string name;
-// user n.
-cout << "Please enter name";
-cin >> name;
-// введите данные
-cout << "Hello world from " << name;
+include<iostream> include<string> int
+main()
+{
+  cout << "Hello world!";
+  string name;
+  // user n.
+  cout << "Please enter name";
+  cin >> name;
+  // введите данные
+  cout << "Hello world from " << name;
 }
